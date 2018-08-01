4f741626636ca3e118f3b5caf04a9b33266a3453
kolyabamberg <kolyabamberg@gmail.com>
2018-08-01 04:52:42 +0300

    #4 - created user editor modal window, fixes #4.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTableModule } from '@angular/material';
+import { MatDialogModule, MatFormFieldModule, MatInputModule, MatTableModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { MenuComponent} from './menu/menu.component';
 import { AppRoutingModule } from './app-routing.module';
 import { UserListComponent } from './user-list/user-list.component';
+import { UserEditorComponent } from './user-editor/user-editor.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    UserListComponent
+    UserListComponent,
+    UserEditorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatTableModule,
+    MatDialogModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
+  entryComponents: [AppComponent, UserListComponent, UserEditorComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,8 +1,13 @@
 export class User {
+  id: number;
   email: string;
   name: string;
+  lastname: string;
   type: string;
   company: string;
   country: string;
   subscription: Date;
+  validated: boolean;
+  level: number;
+  coins: number;
 }
